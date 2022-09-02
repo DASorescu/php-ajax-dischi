79ca3866751543fb74c80da174c08f5e13a4dcb1
github_DASorescu <srsdln97@gmail.com>
2022-09-02 09:21:58 +0200

    small-refactoring

--- dischi-vue/js/script.js
+++ dischi-vue/js/script.js
@@ -4,11 +4,16 @@ const app = new Vue({
     el: '#root',
     data: {
         discs: [],
-
+        generi: [],
     },
     mounted() {
         axios.get('http://localhost/api/discs/discs.php').then(res => {
             this.discs = res.data
+            for (let disc of this.discs) {
+                if (!this.generi.includes(disc.genre)) {
+                    this.generi.push(disc.genre)
+                }
+            }
         });
     }
 });
